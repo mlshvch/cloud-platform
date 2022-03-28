609bc3862109ed6fe7fce281aefe677ad6136b91
mlshvch <malashevichdev@gmail.com>
2022-03-28 15:48:42 +0300

    Add token to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  include DeviseTokenAuth::Concerns::User
+  extend Devise::Models
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable, :lockable, :timeoutable, :trackable,
          :omniauthable
-  include DeviseTokenAuth::Concerns::User
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_28_063908) do
+ActiveRecord::Schema.define(version: 2022_03_28_115040) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,9 @@ ActiveRecord::Schema.define(version: 2022_03_28_063908) do
     t.string "username"
     t.string "first_name", default: "", null: false
     t.string "last_name", default: "", null: false
+    t.string "provider", default: "email", null: false
+    t.string "uid", default: "", null: false
+    t.json "tokens"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
