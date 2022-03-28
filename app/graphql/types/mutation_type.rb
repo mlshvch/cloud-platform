57198c1a47a8575f3d1764ddc974335c940c5954
mlshvch <malashevichdev@gmail.com>
2022-03-28 12:08:01 +0300

    Add SignUp to Mutation Type

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,11 +3,6 @@
 module Types
   # MutationType class
   class MutationType < Types::BaseObject
-    # TODO: remove me
-    field :test_field, String, null: false,
-                               description: 'An example field added by the generator'
-    def test_field
-      'Hello World'
-    end
+    field :sign_up, mutation: Mutations::SignUpUser
   end
 end
