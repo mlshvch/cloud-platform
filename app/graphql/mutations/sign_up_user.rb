3ab4d36289d9f795058c21d0179e55d4b26e2e59
mlshvch <malashevichdev@gmail.com>
2022-03-28 12:06:35 +0300

    Add SignUp Mutation

---
+++ app/graphql/mutations/sign_up_user.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+module Mutations
+  class SignUpUser < BaseMutation
+    # TODO: define return fields
+    # field :post, Types::PostType, null: false
+
+    # TODO: define arguments
+    # argument :name, String, required: true
+    argument :email, String, required: true
+    argument :first_name, String, required: true
+    argument :last_name, String, required: true
+    argument :username, String, required: false
+    argument :password, String, required: true
+    argument :password_confirmation, String, required: true
+
+    field :token, String, null: false
+    field :user, Types::UserType
+
+    def resolve(**params)
+      sign_up_user(params)
+    end
+
+    private
+
+    def sign_up_user(params)
+      User.create!(params)
+    end
+  end
+end
