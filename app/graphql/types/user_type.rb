10aa47f215cb2354c363c7333ca2fd1b83743bf6
mlshvch <malashevichdev@gmail.com>
2022-03-28 12:05:59 +0300

    Create User Type

---
+++ app/graphql/types/user_type.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module Types
+  class UserType < Types::BaseObject
+    description 'Service User'
+    field :id, Integer, null: false
+    field :email, String, null: false
+    field :first_name, String, null: false
+    field :last_name, String, null: false
+    field :username, String, null: true
+    field :password, String, null: false
+    field :confirm_password, String, null: false
+  end
+end
