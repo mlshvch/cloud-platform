dbeb8414062c86b8a6b4e5b21894bc5143e41304
mlshvch <malashevichdev@gmail.com>
2022-05-22 10:12:54 +0300

    Stable deployment without database

--- server/graphql-api/app/sidekiq/configure_service_job.rb
+++ server/graphql-api/app/sidekiq/configure_service_job.rb
@@ -3,79 +3,80 @@
 require 'fullstack_application_service'
 
 class ConfigureServiceJob
-	include Sidekiq::Job
-	include FullstackApplicationService
+  include Sidekiq::Job
+  include FullstackApplicationService
 
-	# TODO REWRITE INSTANCE VARIABLES TO PARAMS_DATA ARRAY
-	
-	def perform(params)
-		change_state('preparing')
-		prepare_data(params)
-		pull
-		create_images
-		connect_database if params['database']
-		create_service
-		delete_redis_record
-		save_data
-	end
+  # TODO REWRITE INSTANCE VARIABLES TO PARAMS_DATA ARRAY
 
-	private
+  def perform(params)
+    change_state('preparing')
+    prepare_data(params)
+    pull
+    create_images
+    connect_database if params['database']
+    create_service
+    delete_redis_record
+  end
 
-	def pull
-		change_state('pulling')
-		root = "#{Rails.root}/user_services"
-		@dir = "#{root}/#{user_to_hex}"
-		dir_name = @app.source.split('/')[-1].split('.')[0]
-		FileUtils.mkdir(@dir) unless Dir.exist?(@dir)
-		FileUtils.cd(@dir)
-		@dir = "#{@dir}/#{dir_name}"
-		git = Git.clone(@app.source, @dir)
-		@dir = git.dir.path
-	end
+  private
 
-	def create_images
-		change_state('creating_images')
-		@app.create_dockerfile('Rails', "#{@dir}")
-		@app.image_id = create_image("#{@dir}", @name)
-	end
+  def pull
+    change_state('pulling')
+    root = "#{Rails.root}/user_services"
+    @dir = "#{root}/#{user_to_hex}"
+    dir_name = @app.source.split('/')[-1].split('.')[0]
+    FileUtils.mkdir(@dir) unless Dir.exist?(@dir)
+    FileUtils.cd(@dir)
+    @dir = "#{@dir}/#{dir_name}"
+    return if Dir.exist?(@dir)
 
-	def connect_database
-		change_state('connecting_database')
-		sleep 30
-	end
+    Git.clone(@app.source, @dir)
+  end
 
-	def create_service
-		change_state('creating_service')
-		parse_yaml(user_to_hex[0..10], @dir, @name, @database)
-		`kubectl apply -f #{@dir}/pod.yml`
-	end
+  def create_images
+    change_state('creating_images')
+    @app.create_dockerfile('Rails', "#{@dir}")
+    @app.image_id = create_image("#{@dir}", @name)
+  end
 
-	def user_to_hex
-		Digest::SHA2.hexdigest @service.user_id.to_s
-	end
+  def connect_database
+    change_state('connecting_database')
+    sleep 30
+  end
 
-	def change_state(state)
-		Redis.new.set("sidekiq_job_#{jid}", state)
-	end
+  def create_service
+    change_state('creating_service')
+    parse_yaml(user_to_hex[0..10], @dir, @name, @database)
+    pp "#{@dir}/pod.yml"
+    `kubectl apply -f #{@dir}/pod.yml` && print("POD STARTED")
+  end
 
-	def delete_redis_record
-		Redis.new.del("sidekiq_job_#{jid}")
-	end
+  def user_to_hex
+    Digest::SHA2.hexdigest @service.user_id.to_s
+  end
 
-	def save_data
-		@app = @app.save!
-		@service.serviceable = @app
-		@service.save!
-	end
+  def change_state(state)
+    Redis.new.set("sidekiq_job_#{jid}", state)
+  end
 
-	def prepare_data(params)
-		@app = FullstackApplication.new
-		@service = Service.new
-		@service.user = User.find_by(id: params['uid'])
-		@app.source = params['repo']
-		@name = params['name']
-		@service.name = @name
-		@database = params['database']
-	end
+  def delete_redis_record
+    Redis.new.del("sidekiq_job_#{jid}")
+  end
+
+  def save_data
+    @app = @app.save!
+    @service.serviceable = @app
+    @service.save!
+  end
+
+  def prepare_data(params)
+    @app = FullstackApplication.new
+    @service = Service.new
+    @service.user = User.find_by(id: params['uid'])
+    @app.source = params['repo']
+    @name = params['name']
+    @service.name = @name
+    @database = params['database']
+  end
 
 end
--- server/graphql-api/lib/fullstack_application_service.rb
+++ server/graphql-api/lib/fullstack_application_service.rb
@@ -1,47 +1,53 @@
 module FullstackApplicationService
-	def create_dockerfile(technology, directory)
-		dockerfile = "#{technology}.#{self.model_name.name}"
-		FileUtils.cp("#{Rails.root}/lib/dockerfiles/#{dockerfile}", directory)
-		FileUtils.mv("#{directory}/#{dockerfile}", "#{directory}/Dockerfile")
-	end
+  def create_dockerfile(technology, directory)
+    dockerfile = "#{technology}.#{self.model_name.name}"
+    FileUtils.cp("#{Rails.root}/lib/dockerfiles/#{dockerfile}", directory)
+    FileUtils.mv("#{directory}/#{dockerfile}", "#{directory}/Dockerfile")
+  end
 
-	def create_image(directory, name)
-		image = Docker::Image.build_from_dir(directory)
-		image.tag('repo' => "localhost:5000/#{name}", 'tag' => 'latest', force: true)
-		image.push
-		image
-	end
+  def create_image(directory, name)
+    return if Docker::Image.exist?("localhost:5000/#{name}:latest")
 
-	def parse_yaml(username, directory, name, database_required)
-		FileUtils.cp("#{Rails.root}/../../k8s/pod.yml", directory)
-		yaml = YAML.load_file("#{directory}/pod.yml")
-		yaml['items'][0]['metadata']['name'] = "#{name}-deployment"
-		yaml['items'][0]['spec']['selector']['matchLabels']['app'] = "#{username}-#{name}"
-		yaml['items'][0]['spec']['template']['metadata']['labels']['app'] = "#{username}-#{name}"
-		yaml['items'][0]['spec']['template']['spec']['containers'][0]['name'] = "#{username}-#{name}"
-		yaml['items'][0]['spec']['template']['spec']['containers'][0]['image'] = "localhost:5000/#{name}"
-		if database_required
-			yaml['items'][0]['spec']['template']['spec']['containers'].append(add_database(database_required, username))
-		end
-		yaml['items'][1]['metadata']['name'] = "#{name}-service"
-		File.open("#{directory}/pod.yml", 'w') { |f| YAML.dump(yaml, f) }
-	end
+    image = Docker::Image.build_from_dir(directory)
+    image.tag('repo' => "localhost:5000/#{name}", 'tag' => 'latest', force: true)
+    image.push
+    image
+  end
 
-	def add_database(db, name)
-		case db
-		when 'postgres'
-			port = 5432
-			name += '-postgres'
-			image = 'postgres'
-		when 'mysql'
-			port = 3306
-			name += '-mysql'
-			image = 'mysql'
-		else
-			return
-		end
-		{ "name" => "#{name.to_s}", "image" => "#{image}",
-		  "ports" => [{ "containerPort" => port, "name" => "#{image}-port" }] }
-	end
+  def parse_yaml(username, directory, name, database_required)
+    yaml = YAML.load_file("#{Rails.root}/../../k8s/pod.yaml")
+    yaml['items'][0]['metadata']['name'] = "#{name}-deployment"
+    yaml['items'][0]['metadata']['labels']['app'] = "#{username}-#{name}"
+    yaml['items'][0]['spec']['selector']['matchLabels']['app'] = "#{username}-#{name}"
+    yaml['items'][0]['spec']['template']['metadata']['labels']['app'] = "#{username}-#{name}"
+    yaml['items'][0]['spec']['template']['metadata']['name'] = "#{username}-#{name}"
+    yaml['items'][0]['spec']['template']['spec']['containers'][0]['name'] = "#{username}-#{name}"
+    yaml['items'][0]['spec']['template']['spec']['containers'][0]['image'] = "localhost:5000/#{name}"
+    yaml['items'][0]['spec']['selector']['matchLabels']['app'] = "#{username}-#{name}"
+    if database_required
+      yaml['items'][0]['spec']['template']['spec']['containers'].append(add_database(database_required, username))
+    end
+    yaml['items'][1]['metadata']['name'] = "#{name}-service"
+    yaml['items'][1]['spec']['selector']['app'] = "#{username}-#{name}"
+
+    File.open("#{directory}/pod.yml", 'w+') { |f| YAML.dump(yaml, f) }
+  end
+
+  def add_database(db, name)
+    case db
+    when 'postgres'
+      port = 5432
+      name += '-postgres'
+      image = 'postgres'
+    when 'mysql'
+      port = 3306
+      name += '-mysql'
+      image = 'mysql'
+    else
+      return
+    end
+    { 'name' => name.to_s, 'image' => image.to_s,
+      'ports' => [{ 'containerPort' => port, 'name' => "#{image}-port" }] }
+  end
 
 end
